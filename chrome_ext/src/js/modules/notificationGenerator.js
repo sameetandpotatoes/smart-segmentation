356dde5173c1f2c6c66951990b64da7afb8a890f
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-03-30 11:15:18 -0500

    Add the Smart Segmentation icon

--- chrome_ext/src/js/modules/notificationGenerator.js
+++ chrome_ext/src/js/modules/notificationGenerator.js
@@ -1,7 +1,8 @@
 function createNotif(title, messageContent) {
   chrome.notifications.create(title.split(' ').join(' '), {
     type: 'basic',
-    iconUrl: 'https://cdn.business2community.com/wp-content/uploads/2018/01/segmentation_1515384711.png',
+    iconUrl: '../images/icon.png',
+    // iconUrl: 'https://cdn.business2community.com/wp-content/uploads/2018/01/segmentation_1515384711.png',
     title: title,
     message: messageContent
   });
