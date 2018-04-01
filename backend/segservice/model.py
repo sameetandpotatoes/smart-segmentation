a599660569acd69a43bbde7c679253b268eb0ff0
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-01 12:34:20 -0500

    Reorganize structure and prioritize phrase length and # of words in phrase as initial factors

--- backend/segservice/model.py
+++ backend/segservice/model.py
@@ -3,15 +3,23 @@ def eval_phrase(segmentation_result, user_selected, record_text, match_phrases, 
     num_words_in_phrase = len(phrase.split(" "))
     phrase_len = len(phrase)
 
-    score = (0.01 * phrase_len)
-    if phrase in match_phrases:
-        score = 0.3 + score / (0.75 * num_words_in_phrase)
-    elif any([partial in phrase for partial in user_selected.lower().split(" ")]):
-        score += 0.1
+    if phrase in match_phrases: # full match
+        segmentation_result['type'] = 'full_match'
+        K = 100.0
+    elif any([partial in phrase for partial in user_selected.lower().split(" ")]): # partial match
+        # TODO improve so it's based on how many matches / length of matches, etc.
+        segmentation_result['type'] = 'partial_match'
+        K = 1.5
+    else: # no match
+        K = 0
+        segmentation_result['type'] = 'no_match'
+        pass
+
+    score = K + (0.01 * phrase_len) / (num_words_in_phrase ** 1.15)
 
     begin_phrase = record_text.lower().find(phrase)
     segmentation_result['formatted_phrase'] = record_text[begin_phrase:begin_phrase + phrase_len]
-    segmentation_result['score'] = score
+    segmentation_result['score'] = "{:.2f}".format(score)
     return score
 
 def get_smart_segmentations(segmentations, user_selected, full_line):
--- backend/segservice/phrase_detection.py
+++ backend/segservice/phrase_detection.py
@@ -16,7 +16,7 @@ def get_phrases_from_sentence(raw_page_text, sentence):
 
     sentence = sentence.encode("utf-8").decode() # fixed platform non-ascii encoding issues
     sent = sentence.lower().split()
-    all_segmentations = get_phrases_original(sentence, sentence_stream, sent)
+    all_segmentations = get_phrases(sentence, sentence_stream, sent)
 
     # Remove duplicates by converting to a set before returning
     return list(set(all_segmentations))
@@ -25,8 +25,8 @@ def get_phrases(sentence, sentence_stream, words):
     stop_words = get_stop_words('english')
     bigram = Phrases(sentence_stream, min_count=1, delimiter=b' ', common_terms = stop_words)
     # TODO refactor to be n-gram
-    trigram = Phrases(bigram[sentence_stream], min_count=1, delimiter=b' ')
-    quadgram = Phrases(trigram[sentence_stream], min_count=1, threshold=0.001, delimiter=b' ')
+    trigram = Phrases(bigram[sentence_stream], min_count=1, delimiter=b' ', common_terms = stop_words)
+    quadgram = Phrases(trigram[sentence_stream], min_count=1, threshold=0.001, delimiter=b' ', common_terms = stop_words)
     all_segmentations = []
 
     bigrams_ = [b for b in bigram[words] if b.count(' ') >= 1]
