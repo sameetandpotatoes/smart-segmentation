a09c6d715377cff11deb111b359b5b24e40a7b24
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-05 17:46:01 -0500

    Add right-clicking for other non-links too

--- chrome_ext/src/js/modules/rightClickListener.js
+++ chrome_ext/src/js/modules/rightClickListener.js
@@ -1,56 +1,44 @@
 import $ from 'jquery';
-import { getRightClickedTextFromEvent } from './textUtils';
+import { wrapHTMLString,
+    getRecordTextFromEvent,
+    getRightClickedTextFromEvent } from './textUtils';
 
 function enableRightClickListener(sendSegEvent) {
-    // $('.a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal').lettering();
     document.oncontextmenu = function(e) {
-        debugger;
+        let wordSelected = $(e.target).text().trim();
+        let recordText = getRecordTextFromEvent(e);
+
+        if (wordSelected.split(" ").length != 1) {
+            var s = window.getSelection();
+            var range = s.getRangeAt(0);
+            var node = s.anchorNode;
+            while(range.startOffset != 0 && range.toString().indexOf(' ') != 0) {
+                range.setStart(node,(range.startOffset -1));
+            }
+            if (range.startOffset != 0) {
+                range.setStart(node, range.startOffset + 1);
+            }
+
+            do {
+               range.setEnd(node, range.endOffset + 1);
+            } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '' &&
+                    range.endOffset < node.length);
+            var str = range.toString().trim();
+            wordSelected = str;
+        }
+
+        // TODO remove punctuation from wordSelected
+        console.log(wordSelected);
+        console.log(recordText);
     }
-}
-
-var wrapContent = (function() {
-  var oSpan = document.createElement('span');
-  oSpan.className = 'mySpanClass';
-
-  var innerFunc = function(id) {
-    var el = (typeof id == 'string')? document.getElementById(id) : id;
-    var node, nodes = el && el.childNodes;
-    var span;
-
-    for (var i=0, iLen=nodes.length; i<iLen; i++) {
-      node = nodes[i];
-      if (node.nodeType == 3) {
-        // node.parentNode.insertBefore(span, node);
-        // span.appendChild(node);
-        var words = node.nodeValue.split(" ");
-        var parentNode = node.parentNode;
-        parentNode.removeChild(node);
-        words.forEach(function(word) {
-            span = oSpan.cloneNode(false);
-            span.appendChild(document.createTextNode(word + " "));
-            parentNode.appendChild(span);
-            // node.parentNode.insertBefore(span, document.createTextNode(word));
-            // span.appendChild(node);
-        });
-      } else {
-        innerFunc(node);
-      }
-    }
-  };
-  return innerFunc;
-}());
-
-function wrapHTMLstring(s) {
-  var el = document.createElement('div');
-  el.innerHTML = s;
-  wrapContent(el);
-  return el.innerHTML;
 }
 
 $(document).ready(function() {
-    $("a").each(function() {
+    $('a').each(function() {
         if (this.innerHTML !== "") {
-            this.innerHTML = wrapHTMLstring(this.innerHTML);
+            // console.log(wrapHTMLString($('body').html()));
+            // $('body').html(wrapHTMLString($('body').html()));
+            this.innerHTML = wrapHTMLString(this.innerHTML);
         }
     });
 });
