0d6f1a4af4aaa2c483c490a6f2fb0f410b69de90
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-08 12:37:21 -0500

    Move evalmodel.py to the same level as devserver

--- backend/evalmodel.py
+++ backend/evalmodel.py
@@ -1,9 +1,6 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join('..', '')))
-from phrase_detection import get_phrases_from_sentence
-from model import get_smart_segmentations, only_full_match
+from segservice.phrase_detection import get_phrases_from_sentence
+from segservice.model import get_smart_segmentations, only_full_match
 import yaml
-import IPython
 
 GREEN = '\033[92m'
 RED = '\033[91m'
@@ -12,11 +9,11 @@ END = '\033[0m'
 
 print_map = True
 
-with open('raw_page_data.dat') as file:
+with open('data/raw_page_data.dat') as file:
     data_stream = file.read().strip()
 
 try:
-    products = yaml.load(open('test_segs.yaml'))
+    products = yaml.load(open('data/test_segs.yaml'))
 except yaml.YAMLError as e:
     print("Parsing YAML string failed")
     print("Reason:", e.reason)
