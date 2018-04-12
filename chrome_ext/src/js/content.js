54e79a976c17267face48c22879da2106a77721b
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-11 22:37:19 -0500

    Address #14

--- chrome_ext/src/js/content.js
+++ chrome_ext/src/js/content.js
@@ -53,4 +53,8 @@ enableRightClickListener(handleSegmentation);
 // TODO uncomment when we have a storage model implemented so we can store text and not send it per request
 currentTextOnPage = getTextOnCurrentPage();
 let currentUrl = window.location.href;
-sendPayloadToBackend({cleanedText: currentTextOnPage, currentPage: currentUrl});
+sendPayloadToBackend({cleanedText: currentTextOnPage, currentPage: currentUrl},
+    function(response) {
+        // empty
+    }
+);
