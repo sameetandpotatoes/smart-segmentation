86a2f95c47241cfd9d2e7336bac886d4be75be37
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-18 14:47:48 -0500

    Fix bugs in reconstruct algo

--- backend/segservice/model.py
+++ backend/segservice/model.py
@@ -50,8 +50,6 @@ class SmartSegmenter:
     def get_phrases_from_sentence(self, sentence, debug=True):
         words = sentence.lower().split()
 
-        # Remove non-ascii characters so strings can be printed
-        #sentence = sentence.encode('utf-8').decode()
         sentence, num_list = clean_data(sentence)
         sent = sentence.lower().split()
         all_phrases = self.get_phrases(sentence, sent)
@@ -59,56 +57,67 @@ class SmartSegmenter:
             print(all_phrases)
         return all_phrases, num_list
 
+    def reconstruct_original_phrase(self, record_text, phrase):
+        phrase_list = phrase.split('####')
+        og_phrase = ""
+        # Match the exact phrase in the rt lower to prevent getting anything before
+        rt_lower_og = record_text.lower()
+        rt_lower = re.sub(r'\d+', number_replacement, record_text.lower())
+        # Add buffer to go back based on how many hashtags were entered
+        record_before_phrase = rt_lower[0:rt_lower.index(phrase)]
+        num_hashtags_before_phrase = record_before_phrase.count("#")
+        rt_lower_start = rt_lower_og[rt_lower.index(phrase) - num_hashtags_before_phrase:]
+        rt_lower = rt_lower_start
+        idx = 0
+        for i, f in enumerate(phrase_list):
+            # Get next element in list if it exists, to determine how many numbers there are
+            s = "" if i + 1 >= len(phrase_list) else phrase_list[i + 1]
+            next_part_of_phrase = ""
+            if f == "":
+                end_idx = rt_lower.index(s)
+                start_idx = end_idx - 1
+                while start_idx > 0 and rt_lower[start_idx].isdigit():
+                    start_idx -= 1
+                if start_idx is not 0:
+                    start_idx += 1 # add back 1 only if the last one was not a digit
+                next_part_of_phrase = rt_lower[start_idx:end_idx]
+            else:
+                start_idx = rt_lower.index(f)
+                end_idx = start_idx + len(f)
+                # Copy string over up to the number
+                next_part_of_phrase = rt_lower[start_idx: end_idx]
+                # Find the first part of second string after the number
+                next_idx = rt_lower[end_idx:].index(s)
+                # Add all numbers to the phrase
+                next_part_of_phrase += rt_lower[end_idx:end_idx + next_idx]
+            idx += len(next_part_of_phrase)
+            og_phrase += next_part_of_phrase
+            # reset rt_lower to after the string
+            rt_lower = rt_lower[rt_lower.find(next_part_of_phrase) + len(next_part_of_phrase):]
+        # If there were no numbers in the phrase, og_phrase is empty so reset it
+        return phrase if og_phrase == "" else og_phrase.strip()
+
     def eval_phrase(self, segmentation_result, user_selected, record_text, match_phrases, nonmatch_phrases, nums_list):
         phrase = segmentation_result['phrase']
+
+        # TODO can be removed
         cleaned_record_text, _ = clean_data(record_text)
         split_nums = cleaned_record_text.split('####')
         phrase_index = cleaned_record_text.find(phrase)
         shift, num_list_index = calculate_shift(nums_list, split_nums, phrase_index)
         phrase_list = phrase.split('####')
         original_phrase = phrase_list[0]
-
-        og_phrase = ""
-        # Match the exact phrase in the rt lower to prevent getting anything before
-        rt_lower_og = record_text.lower()
-        rt_lower = re.sub(r'\d+', number_replacement, record_text.lower())
-        # Add buffer to go back based on how many hashtags were entered
-        num_hashtags_before_phrase = rt_lower[0:rt_lower.index(phrase)].count("#")
-        rt_lower = rt_lower_og[rt_lower.index(phrase) - num_hashtags_before_phrase:]
-        for i, f in enumerate(phrase_list):
-            # Get next element in list if it exists, to determine how many numbers there are
-            s = "" if i + 1 >= len(phrase_list) else phrase_list[i + 1]
-            if f == "":
-                end_idx = rt_lower.index(s)
-                start_idx = end_idx - 1
-                while start_idx > 0 and rt_lower[start_idx].isdigit():
-                    start_idx -= 1
-                start_idx += 1 # add back 1, since it's not a digit
-                og_phrase += rt_lower[start_idx:end_idx]
-            else:
-                start_idx = rt_lower.index(f)
-                end_idx = start_idx + len(f)
-                # Add up to the number
-                og_phrase += rt_lower[start_idx : end_idx]
-                # Find the first part after the number
-                next_idx = rt_lower[end_idx:].index(s)
-                # Add all numbers to the phrase
-                og_phrase += rt_lower[end_idx:end_idx + next_idx]
-        # If there were no numbers in the phrase, og_phrase is empty so reset it
-        og_phrase = phrase if og_phrase == "" else og_phrase
-
-        # TODO alternative approach - decide which is better
         for index,val in enumerate(phrase_list):
             if index is not len(phrase_list)-1:
                 original_phrase += nums_list[num_list_index]
                 num_list_index +=1
                 original_phrase += phrase_list[index+1]
 
-        original_phrase = og_phrase
+        original_phrase = self.reconstruct_original_phrase(record_text, phrase)
         num_words_in_phrase = len(original_phrase.split(" "))
         phrase_len = len(original_phrase)
 
-        begin_phrase = record_text.lower().find(phrase)
+        begin_phrase = record_text.lower().find(original_phrase)
         phrase_num_signs = cleaned_record_text.lower().find(phrase)
         shift, _ = calculate_shift(nums_list, split_nums, phrase_num_signs)
 
@@ -129,8 +138,10 @@ class SmartSegmenter:
         else:
             score = K + (0.01 * phrase_len) / (num_words_in_phrase ** 1.15)
 
+        # TODO remove
+        andy = record_text[(phrase_num_signs - shift):(phrase_num_signs - shift + phrase_len)]
 
-        segmentation_result['formatted_phrase'] = record_text[(phrase_num_signs - shift):(phrase_num_signs - shift + phrase_len)]
+        segmentation_result['formatted_phrase'] = record_text[begin_phrase:begin_phrase+phrase_len]
         segmentation_result['score'] = "{:.2f}".format(score)
         return score
 
